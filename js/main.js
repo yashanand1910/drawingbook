6a2787e82957c020745d6ac7401e9015d7937bf2
bhuvana-24 <b.ravulla@gmail.com>
2018-12-25 13:08:13 +0530

    Adding files

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var $canvas = $("canvas");
 var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
+var bErasing = false;
 
 $("#colors").on("click", "li", function() {
     $(this).siblings().removeClass("selected");
@@ -29,3 +30,44 @@ $canvas.mousedown(function(e) {
 }).mouseleave(function() {
     $canvas.mouseup();
 });
+$('resetcanvas').click(function(context){
+    canvas.getContext('2d').clearRect(0,0,width,height);
+canvas.width = canvas.width;
+});
+
+$("#eraser").click(function() {
+        bErasing = true;
+    });
+
+
+
+var pageNum = 1;
+$("#newpage").click(function(){
+    pageNum = pageNum +1;        
+    $("#container ul").append(`<li class="pageLink" onclick="pageLinkClick(` + pageNum + `)" ctab="` + pageNum + `"><a id="pagelink` + pageNum + `"><span class="tab">Page` + pageNum + `</span></a></li>`);
+    
+    // Create new Canva
+    $("#canvasArea").append(`<center>
+                <canvas id="mycanvas` + pageNum + `" class="mycanvas" width="800" height="500"></canvas>
+            </center> `)
+    
+    //Hide every other canvas
+    $("#canvasArea").find('.mycanvas').removeClass('active'); // Still need to be worked on.    
+
+});
+
+//TODO 
+function pageLinkClick(linkNumber) {
+    
+    
+    alert("page number: " + linkNumber + " clicked")
+    
+    
+    
+    
+   
+    
+}
+
+
+
