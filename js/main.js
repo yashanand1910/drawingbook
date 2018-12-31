d73cdcee63b6d13c500bc38e4df6ab32bc4df4b4
sharah12 <sharah.fathima12@gmail.com>
2018-12-31 16:38:33 +0530

    Added tabs

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@ var lastEvent;
 var mouseDown = false;
 var bErasing = false;
 
+//clearing canvas
+ document.getElementById('clearcanvas').addEventListener('click', function() {
+     context.clearRect(0,0, 800,500);}, false);
+
+//new window
+function myFunction() {
+    var mywindow = window.open("index.html", "", "width=200,height=100");
+}
+
 $("#colors").on("click", "li", function() {
     $(this).siblings().removeClass("selected");
     $(this).addClass("selected");
@@ -31,47 +40,6 @@ $canvas.mousedown(function(e) {
     $canvas.mouseup();
 });
 
-//clearing canvas
- document.getElementById('clearcanvas').addEventListener('click', function() {
-     context.clearRect(0,0, 800,500);}, false);
-
-
-
-
-
-
-<<<<<<< HEAD
-
-//switching to multiple tabs
-=======
->>>>>>> b7244f587df6e3eec42eef02ce66b5b87bac78a1
-var pageNum = 1;
-$("#newpage").click(function(){
-    pageNum = pageNum +1;        
-    $("#container ul").append(`<li class="pageLink" onclick="pageLinkClick(` + pageNum + `)" ctab="` + pageNum + `"><a id="pagelink` + pageNum + `"><span class="tab">Page` + pageNum + `</span></a></li>`);
-    
-    // Create new Canva
-    $("#canvasArea").append(`<center>
-                <canvas id="mycanvas` + pageNum + `" class="mycanvas" width="800" height="500"></canvas>
-            </center> `)
-    
-    //Hide every other canvas
-    $("#canvasArea").find('.mycanvas').removeClass('active'); // Still need to be worked on.    
-
-});
-
-//TODO 
-function pageLinkClick(linkNumber) {
-    
-    
-    alert("page number: " + linkNumber + " clicked")
-    
-    
-    
-    
-   
-    
-}
 
 
 
