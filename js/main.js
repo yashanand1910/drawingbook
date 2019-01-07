58f2ae822ab341b217196555c6644b93053d395b
sharah12 <sharah.fathima12@gmail.com>
2019-01-07 16:58:42 +0530

    Added tabs

--- js/main.js
+++ js/main.js
@@ -5,14 +5,35 @@ var lastEvent;
 var mouseDown = false;
 var bErasing = false;
 
+//tabs
+$(document).ready(function () {
+            var tabID = 1;
+            $('#btn-add-tab').click(function () {
+                tabID++;
+                $('#tab-list').append($('<li><a href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab' + tabID + '<button class="close" type="button" title="Remove this page">x</button></a></li>'));
+                $('#tab-content').append($('<div class="tab-pane fade" id="tab' + tabID + '">Tab '+ tabID +' content</div>'));
+            });
+            $('#tab-list').on('click','.close',function(){
+                var tabID = $(this).parents('a').attr('href');
+                $(this).parents('li').remove();
+                $(tabID).remove();
+
+                //display first tab
+               var tabFirst = $('#tab-list a:first');
+                 tabFirst.tab('show');
+            });
+            
+            var list = document.getElementById("tab-list");
+        });
+
 //clearing canvas
  document.getElementById('clearcanvas').addEventListener('click', function() {
      context.clearRect(0,0, 800,500);}, false);
 
-//new window
+/*new window
 function myFunction() {
     var mywindow = window.open("index.html");
-}
+}*/
 
 //implementing eraser
 function eraser() {
