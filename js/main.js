e91dd2d385338b7f9977b12dacc64ba81b7247d2
sharah-dev <sharah.fathima12@gmail.com>
2019-01-12 18:39:04 +0530

    edited code

--- js/main.js
+++ js/main.js
@@ -4,15 +4,27 @@ var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
 var bErasing = false;
-
+$('#canvas1').click(function () {
+	debugger;
+	style.hide();
+});
 //tabs
 $(document).ready(function () {
             var tabID = 1;
             $('#btn-add-tab').click(function () {
                 tabID++;
                 $('#tab-list').append($('<li><a href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab' + tabID + '<button class="close" type="button" title="Remove this page">x</button></a></li>'));
-                $('#tab-content').append($('<div class="tab-pane fade" id="tab' + tabID + '">Tab '+ tabID +' content</div>'));
+                $('#tab-content').append($('<div class="tab-pane fade" id="tab' + tabID +'">Tab '+ tabID +' content</div>'));
+                var id ="tab" + tabID ;
+                var x=document.createElement("canvas");
+                x.style.border="1px solid black";
+                x.style.width="800px";
+                x.id="canvas"+tabID;
+                x.style.height="500px";
+                document.getElementById("canvasAreaCenter").appendChild(x);
+                hideOtherTabs(tabID);
             });
+
             $('#tab-list').on('click','.close',function(){
                 var tabID = $(this).parents('a').attr('href');
                 $(this).parents('li').remove();
@@ -25,6 +37,8 @@ $(document).ready(function () {
             
             var list = document.getElementById("tab-list");
         });
+
+//multiple tabs
 
 //clearing canvas
  document.getElementById('clearcanvas').addEventListener('click', function() {
@@ -39,7 +53,14 @@ function myFunction() {
 function eraser() {
     return color="white";
 }
-
+function hideOtherTabs(j){
+    var tabs = document.getElementsByTagName('canvas');
+    for(var i=0;i<tabs.length;i++){
+        tabs[i].classList='mycanvas';
+        tabs[i].classList.add('hide');
+    }
+   document.getElementById('canvas'+j).classList='mycanvas';
+}
 $("#colors").on("click", "li", function() {
     $(this).siblings().removeClass("selected");
     $(this).addClass("selected");
