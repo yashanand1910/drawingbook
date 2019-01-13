f4b9b2aa6246d2523e8ef1334f22d9eb92ca8c95
sharah-dev <sharah.fathima12@gmail.com>
2019-01-13 20:15:11 +0530

    edited code

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
+
 var color = $(".selected").css("background-color");
 var $canvas = $("canvas");
 var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
 var bErasing = false;
-$('#canvas1').click(function () {
-	debugger;
-	style.hide();
-});
+
 //tabs
 $(document).ready(function () {
             var tabID = 1;
             $('#btn-add-tab').click(function () {
                 tabID++;
-                $('#tab-list').append($('<li><a href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab' + tabID + '<button class="close" type="button" title="Remove this page">x</button></a></li>'));
-                $('#tab-content').append($('<div class="tab-pane fade" id="tab' + tabID +'">Tab '+ tabID +' content</div>'));
+                 $('#tab-list').append($('<li class="topTab" id="tab  '+tabID+'" ><a id='+tabID+'href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab' + tabID + '<button class="close" type="button" title="Remove this page">x</button></a></li>'));
+                $('#tab-content').append($('<div class="tab-pane fade " id="tab' + tabID +'">Tab '+ tabID +' content</div>'));
                 var id ="tab" + tabID ;
                 var x=document.createElement("canvas");
                 x.style.border="1px solid black";
                 x.style.width="800px";
                 x.id="canvas"+tabID;
-                x.style.height="500px";
+                x.style.height="1500";
                 document.getElementById("canvasAreaCenter").appendChild(x);
                 hideOtherTabs(tabID);
+                tabSelected(tabID);
             });
 
             $('#tab-list').on('click','.close',function(){
@@ -34,12 +33,12 @@ $(document).ready(function () {
                var tabFirst = $('#tab-list a:first');
                  tabFirst.tab('show');
             });
-            
+
             var list = document.getElementById("tab-list");
         });
 
 //multiple tabs
-
+              
 //clearing canvas
  document.getElementById('clearcanvas').addEventListener('click', function() {
      context.clearRect(0,0, 800,500);}, false);
@@ -49,6 +48,11 @@ function myFunction() {
     var mywindow = window.open("index.html");
 }*/
 
+function tabSelected(id){
+     $canvas = $("canvas");
+    context= document.getElementById("canvas"+id).getContext("2d");
+    eventAddition();
+}
 //implementing eraser
 function eraser() {
     return color="white";
@@ -59,14 +63,16 @@ function hideOtherTabs(j){
         tabs[i].classList='mycanvas';
         tabs[i].classList.add('hide');
     }
-   document.getElementById('canvas'+j).classList='mycanvas';
+   var cId='canvas'+j.trim();
+   document.getElementById(cId).classList='mycanvas';
 }
 $("#colors").on("click", "li", function() {
     $(this).siblings().removeClass("selected");
     $(this).addClass("selected");
     color = $(".selected").css("background-color");
 });
-
+eventAddition();
+function eventAddition(){
 $canvas.mousedown(function(e) {
     lastEvent = e;
     mouseDown = true;
@@ -86,6 +92,11 @@ $canvas.mousedown(function(e) {
 }).mouseleave(function() {
     $canvas.mouseup();
 });
+ $('.topTab').click(function (e) {
+                 var id=e.currentTarget.id.substring(3);
+                   hideOtherTabs(id); 
+              });
+}
 
 
 
