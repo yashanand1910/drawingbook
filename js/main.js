304bc0a50af9c16f3a77be102a21672ac1624850
Sri Vidya <poshala.srividya@gmail.com>
2019-01-16 23:59:09 +0530

    Fixed tab bugs

--- js/main.js
+++ js/main.js
@@ -5,17 +5,20 @@ var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
 var bErasing = false;
+var tabsAvailable=[]; // step1
 
 //tabs
 $(document).ready(function () {
             var tabID = 1;
+            tabsAvailable.push(1);  // step 2 
             $('#btn-add-tab').click(function () {
-                tabID++;
+                tabID = getTabNumber(tabsAvailable); // step 3 
+                tabsAvailable.push(tabID); // step 4
                  $('#tab-list').append($('<li class="topTab" id="tab'+tabID+'" ><a id='+tabID+'href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab' + tabID + '<button class="close" type="button" title="Remove this page">x</button></a></li>'));
                 $('#tab-content').append($('<div class="tab-pane fade " id="tab' + tabID +'">Tab '+ tabID +' content</div>'));
                 var id ="tab" + tabID ;
                 var x=document.createElement("canvas");
-                x.border="2px solid black";
+                x.border="1px solid black";
                 x.width="800";
                 x.id="canvas"+tabID;
                 x.height="500";
@@ -26,9 +29,11 @@ $(document).ready(function () {
 
             $('#tab-list').on('click','.close',function(){
                 var tabID = $(this).parents('a').attr('href');
+                tabID = $(this).parents('a')[0].innerText.charAt(3)
+                removeTab(tabID); // step 5  
                 $(this).parents('li').remove();
                 $(tabID).remove();
-
+                tabSelected(tabsAvailable[0]);//step8
                 //display first tab
                var tabFirst = $('#tab-list a:first');
                  tabFirst.tab('show');
@@ -36,18 +41,11 @@ $(document).ready(function () {
 
             var list = document.getElementById("tab-list");
         });
-
-//multiple tabs
               
 //clearing canvas
- document.getElementById('clearcanvas').addEventListener('click', function() {
-     context.clearRect(0,0, 800,500);}, false);
-
-/*new window
-function myFunction() {
-    var mywindow = window.open("index.html");
-}*/
-
+     document.getElementById('clearcanvas').addEventListener('click', function() {
+        context.clearRect(0,0, 800,500);}, false);
+//selection of tabs
 function tabSelected(id){
      $canvas = $("canvas");
     context= document.getElementById("canvas"+id).getContext("2d");
@@ -56,9 +54,20 @@ function tabSelected(id){
 //implementing eraser
 function eraser() {
     return color="white";
-
 }
-
+// step 6
+function removeTab(tabID){
+    var index  = tabsAvailable.indexOf(parseInt(tabID));
+    tabsAvailable.splice(index,1);
+}
+// step 7 
+function getTabNumber(tabsAvailable){
+    var highest=0;
+    tabsAvailable.forEach(element => {
+        highest=element>highest?element:highest;
+    });
+    return highest+1;
+}
 function hideOtherTabs(j){
     var tabs = document.getElementsByTagName('canvas');
     for(var i=0;i<tabs.length;i++){
@@ -100,7 +109,3 @@ $canvas.mousedown(function(e) {
                    tabSelected(id);
               });
 }
-
-
-
-
